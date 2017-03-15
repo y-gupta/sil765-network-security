f6aebce7d199855c035fce2298eca5a2ca5a2731
Ujjwal Sinha <ujjwalsinha79@gmail.com>
2017-03-15 16:29:27 +0530

    Assignment 2 : Document is now a text file. Time stamping done.

--- ass2/main.py
+++ ass2/main.py
@@ -29,24 +29,42 @@ def server(h):
 
 
 def client():
-	document="Python is a great language!!"
+	fname="test.txt"
+	fo=open(fname, "w")
+	fo.write("Python is a great language.\nYeah its great!!\n");
+	fo.close()
+	#Initial Document
+	fobj = open(fname,"r")
+	document = fobj.read()
+	fobj.close()
 	print "Initial Document = "+document
 	h = hashlib.sha1(document).hexdigest() #SHA1 of the document before it's sent to the time stamping server
 	signature= server(h)
 	plain = decrypt(public,signature)
-	document=document+plain
-	print "After Timestamping = "+document
+	fo=open(fname, "a")
+	fo.write(plain);
+	fo.close()
+	#After Timestamping Document
+	f2 = open(fname,"r")
+	mdocument = f2.read()
+	f2.close()
+	print "After Timestamping = "+mdocument
 	print "To verify if the document existed at that point of time"
-	timest = document[-40:] #Timestamp
-	hashdct =document[-80:-40] #SHA1 hash of the document
-	textdct = document[:-80] # text of the document
+	f3 = open(fname,"r")
+	tdocument = f3.readlines()
+	f3.close()
+	vdocument = tdocument[len(tdocument) - 1]
+	timest = vdocument[-40:] #Timestamp
+	hashdct = vdocument[-80:-40] #SHA1 hash of the document
+	textdc = tdocument[:-1] # text of the document
+	textdct=""
+	for i in range(len(textdc)):
+		textdct=textdct+textdc[i]
 	#compare the hash of the present text with the hash generated at the beginning.
 	b = hashlib.sha1(textdct).hexdigest()
 	if b == hashdct:
 		print "Document is Verified"
 	else:
 		print "Document is Modified and Tampered"
-
-
 
 client()
